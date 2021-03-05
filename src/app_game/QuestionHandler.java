9be5b189e116e2e99919d78f3845f0ca84648fb8
Fat-stack <robinnilsson5@msn.com>
2021-03-05 15:03:54 +0100

    Removal of swedish comments and text in program

--- src/app_game/QuestionHandler.java
+++ src/app_game/QuestionHandler.java
@@ -16,22 +16,19 @@ public class QuestionHandler implements Serializable {
     LinkedList<QuestionHandler> questionList1;
     String question;
     List<String> options;
+
                 // CONSTRUCTORS:
-    public  QuestionHandler(){} // tom konstruktor
-    public QuestionHandler(LinkedList questionList){ // konstruktor som används vid skapande av listor för serialisering( Tidigare ListBluePrint)
+    public  QuestionHandler(){}
+    public QuestionHandler(LinkedList questionList){
         this.questionList1 = questionList;}
-    public QuestionHandler(String question, List<String> options){ // konstruktor som används vid skapande av enskilda frågor. (tidigare QuestionBluePrint)
+    public QuestionHandler(String question, List<String> options){
         this.question = question;
         this.options =  options;
-
     }
 
                 // METHODS:
-        public void resetQuestionsFromTextFile() throws IOException {
-        // QUESTIONBANK SETUP (questionsetup-class) merged med initSetUp
-
+    public void resetQuestionsFromTextFile() throws IOException {
         //Method to read txt file and add every question as objects to questionList
-
         //Contains the information from QuestionBank.txt - Each line on an Index
         LinkedList<String> linesFromQuestionBank = new LinkedList<>();
         //List which can hold objects created from QuestionHandler class - Each object on an Index
@@ -46,17 +43,11 @@ public class QuestionHandler implements Serializable {
             options.set(0, options.get(0).replaceFirst(":",""));
             questionList.add(new QuestionHandler(question, options));
         }
-
         writeToSer(questionList);
-
         System.out.println("successfully initiated questionbank from txt-file.");
-        System.out.println("resetQuestionsFromTextFile COMPLETE"); // TA BORT EFTER FUNK.
-
     }
     public void writeToSer(Object toStore) throws IOException {
-
-        //skriver över filen varje gång den används med Objektet vi skickar med(LinkedList, ( Därav viktigt att vi
-        //  hämtar filen, modifierar och sen sparar över den nya versionen )
+        //Overwrites the info on q.ser with the info sent as a parameter in this method.
 
         FileOutputStream fileOutput = new FileOutputStream(serFile);
         ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
@@ -64,70 +55,51 @@ public class QuestionHandler implements Serializable {
         objectOutput.writeObject(toStore);
         objectOutput.flush();
         objectOutput.close();
-
-        System.out.println("WRITE TO SER COMPLETE");
     }
     public LinkedList<QuestionHandler> readBackSer() throws IOException, ClassNotFoundException {
 
         FileInputStream fileInput = new FileInputStream(serFile);
         ObjectInputStream objectInput = new ObjectInputStream(fileInput);
-
-        //Måste hämtas ut i samma format som den skickas in, enligt app_game.ListBluePrint
         QuestionHandler listAfterDeSer = new QuestionHandler((LinkedList) objectInput.readObject());
 
-
-        //System.out.println("succes in readBackser()");
-        // .questionList1 i enlighet med klassen app_game.ListBluePrint
-        System.out.println("readBackSer COMPLETE."); // TA BORT EFTER FUNK.
         return listAfterDeSer.questionList1;
     }
     public void addQuestion() throws IOException, ClassNotFoundException {
-        // ny temporär linkedlist som hämtar  den sparade filens LinkedList.
+        // New temp. linkedList to store info from q.ser(readBackser)
         LinkedList<QuestionHandler> tempLinkedList = readBackSer();
-
 
         LinkedList answerList = new LinkedList();
 
-        // Scanner för att lägga in frågorna i enlighet med app_game.QuestionBluePrint-formatet.
+        // Scanner to insert new question.
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("mata in fråga");
+        System.out.println("Enter new Question:");
         String q = sc.nextLine();
-
-        System.out.println("mata in svar A");
+        System.out.println("Enter answer A:");
         String answer1 = validateOption(sc.nextLine().toLowerCase().trim());
-
-        System.out.println("mata in svar B");
+        System.out.println("Enter answer B:");
         String answer2 = validateOption(sc.nextLine().toLowerCase().trim());
-
-
-        System.out.println("mata in svar C");
+        System.out.println("Enter answer C:");
         String answer3 = validateOption(sc.nextLine().toLowerCase().trim());
-
-        System.out.println("mata in svar D");
+        System.out.println("Enter answer D:");
         String answer4 = validateOption(sc.nextLine().toLowerCase().trim());
 
-        // Lägger till svar i answelist för att skicka vidare via construktorn.
+        // adding answers to list
         answerList.add(answer1);
         answerList.add(answer2);
         answerList.add(answer3);
         answerList.add(answer4);
 
-        // lägger till den skapade frågan in i tempLinkedList,
+        // Add the new question to templinkedlist
         tempLinkedList.add(new QuestionHandler(q, answerList));
 
-
-        //Skriver den modifierade listan till fil.
+        //Write the modified list back to q.ser.
         writeToSer(tempLinkedList);
 
-       System.out.println("Du har lagt till frågan: " + tempLinkedList.getLast().question + tempLinkedList.getLast().options);
-
-        System.out.println("addQuestion COMPLETE"); // TA BORT EFTER FUNK.
-        //öppna lägg till spara printa
-
+       System.out.println("Question added: " + tempLinkedList.getLast().question + tempLinkedList.getLast().options);
     }
     public String validateOption(String strOption){
-        System.out.println("Was this the correct option? - Answer with 'y' or 'n' ( YES or NO)");
+        System.out.println("Is this the correct option? - Answer with 'y' or 'n' ( YES or NO)");
         Scanner scValidate = new Scanner(System.in);
 
         String validationAnswer = scValidate.next().toLowerCase();
@@ -141,48 +113,26 @@ public class QuestionHandler implements Serializable {
 
     }
     public void showAllQuestions() throws IOException, ClassNotFoundException {
-        // Läs upp alla rad för rad med indexering synligt
-
+        // Printing all questions, one row per question with a numeric list in the eyes of the viewer.
         LinkedList<QuestionHandler> tempListReadAll = readBackSer();
-
-
-
         for (int i = 0; i < tempListReadAll.size(); i++) {
-
-
             System.out.println(i +1 + " " +tempListReadAll.get(i).question + " - " + tempListReadAll.get(i).options);
-
-
-
         }
-
     }
     public void removeQuestion() throws IOException,ClassNotFoundException {
-
         Scanner scRemove = new Scanner(System.in);
-
-        // LÄS IN, TA BORT, SPARA
         LinkedList<QuestionHandler> tempList = readBackSer();
 
-        System.out.println("vilken fråga vill du ta bort? (nr i show all questions)");
+        System.out.println("Which question do you want to remove? (number from list of questions)");
         int NumberToRemove = scRemove.nextInt()-1;
         tempList.remove(NumberToRemove);
 
-        //spara över ser-fil
+        //Write modified list to ser-file again.
         writeToSer(tempList);
-
         System.out.println("Successful with removal of question number  " + (NumberToRemove+1));
 
     }
     public void editQuestion() throws IOException, ClassNotFoundException {
-        //1 LÄS IN LIST FRÅN SER
-        //2 POCKA UT VALD INDEX
-        //3 SKAPA EN NY FRÅGA
-        //4 RADERA VALT INDEX
-        //5 LÄGG IN FRÅGA I SAMMA INDEX
-        //6 SPARA PÅ FIL
-
-
         Scanner scEdit = new Scanner(System.in);
         //1 Read questions from .ser
         LinkedList<QuestionHandler> tempList = readBackSer();
@@ -195,23 +145,15 @@ public class QuestionHandler implements Serializable {
 
 
         LinkedList answerList = new LinkedList();
-
-        // Scanner för att lägga in frågorna i enlighet med QuestionBluePrint-formatet.
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter question:");
         String q = sc.nextLine();
-
         System.out.println("Enter answer A");
         String answer1 = validateOption(sc.nextLine().toLowerCase().trim());
-
         System.out.println("Enter answer B");
         String answer2 = validateOption(sc.nextLine().toLowerCase().trim());
-
-
         System.out.println("Enter answer C");
         String answer3 = validateOption(sc.nextLine().toLowerCase().trim());
-
         System.out.println("Enter answer D");
         String answer4 = validateOption(sc.nextLine().toLowerCase().trim());
 
@@ -224,34 +166,12 @@ public class QuestionHandler implements Serializable {
         //4 Remove previous question at this index.
         tempList.remove(numberToEdit);
 
-        // lägger till den skapade frågan in i tempLinkedList,
-        // 5 Add the edited question in the same index-position as the deleted one.
+        // 5 Add the edited(new) question in the same index-position as the deleted one(old one).
         tempList.add(numberToEdit, new QuestionHandler(q,answerList));
-        //System.out.println("Question number " + (numberToEdit + 1) + " is edited to :" + tempList.get(numberToEdit).question + "? " + tempList.get(numberToEdit).options + ".") ;
-        //System.out.println("Question number " + (numberToEdit + 1) + " is edited to :" + newQuestion.question + "? " + newQuestion.options + ".") ;
 
-
-        // Skapar ett Objekt av app_game.ListBluePrint (för att kunna serializera på nytt), där vi skickar med den gamla listan
-        // med en adderad fråga på rätt index-plats.
-        // 6 Create object for serialization and fill it with the edited linkedlist AND save it.
-        //QuestionHandler listToSave = new QuestionHandler(tempList);
-
-
-        //Skriver den nya listan till fil.
-
+        // 6 Write modified list to ser-file
         System.out.println("Question number " + (numberToEdit + 1) + " is edited to :" + tempList.get(numberToEdit).question + "? " + tempList.get(numberToEdit).options + ".") ;
         writeToSer(tempList);
-
-        //  System.out.println("Du har lagt till frågan: " + newQuestion.question + newQuestion.options);
-
-        /////////
-
-
-
-
-
-
-
     }
 
 
